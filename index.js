cc54c300534f5b60d6ad4ea8fbfd2363221c07e3
James Barlow <tentbub@Jamess-MacBook-Pro.local>
2020-11-11 12:35:05 -0700

    message

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+$('#one').click(function() {
+    $('#one').toggleClass("gray")
+})
+
+$('#two').click(function() {
+    $('#two').toggleClass("gray")
+})
+
+$('#three').click(function() {
+    $('#three').toggleClass("gray")
+})
+
+$('#four').click(function() {
+    $('#four').toggleClass("gray")
+})
+
+$('#five').click(function() {
+    $('#five').toggleClass("gray")
+})
+
+$('#six').click(function() {
+    $('#six').toggleClass("gray")
+})
+
+$('#seven').click(function() {
+    $('#seven').toggleClass("gray")
+})
+
+$('#eight').click(function() {
+    $('#eight').toggleClass("gray")
+})
+
+$('#nine').click(function() {
+    $('#nine').toggleClass("gray")
+})
+
+$('#ten').click(function() {
+    $('#ten').toggleClass("gray")
+})
+
+$('#eleven').click(function() {
+    $('#eleven').toggleClass("gray")
+})
+
+$('#twelve').click(function() {
+    $('#twelve').toggleClass("gray")
+})
+
+$('#thirteen').click(function() {
+    $('#thirteen').toggleClass("gray")
+})
+
+$('#fourteen').click(function() {
+    $('#fourteen').toggleClass("gray")
+})
+
+$('#fifteen').click(function() {
+    $('#fifteen').toggleClass("gray")
+})
+
+$('#sixteen').click(function() {
+    $('#sixteen').toggleClass("gray")
+})
